1de401fab0ced8e3a6319f4e603a09feead26be3
Connie <conniekliu95@gmail.com>
2015-08-07 03:32:06 -0400

    updated lots of pics for project pages and project css

--- js/jquery.orbit-1.2.3.js
+++ js/jquery.orbit-1.2.3.js
@@ -13,15 +13,15 @@
 
         //Defaults to extend options
         var defaults = {  
-            animation: 'horizontal-push', 		// fade, horizontal-slide, vertical-slide, horizontal-push
+            animation: 'horizontal-slide', 		// fade, horizontal-slide, vertical-slide, horizontal-push
             animationSpeed: 600, 				// how fast animtions are
             timer: true, 						// true or false to have the timer
-            advanceSpeed: 4000, 				// if timer is enabled, time between transitions 
-            pauseOnHover: false, 				// if you hover pauses the slider
-            startClockOnMouseOut: false, 		// if clock should start on MouseOut
+            advanceSpeed: 3000, 				// if timer is enabled, time between transitions 
+            pauseOnHover: true, 				// if you hover pauses the slider
+            startClockOnMouseOut: true, 		// if clock should start on MouseOut
             startClockOnMouseOutAfter: 1000, 	// how long after MouseOut should the timer start again
             directionalNav: true, 				// manual advancing directional navs
-            captions: true, 					// do you want captions?
+            captions: false, 					// do you want captions?
             captionAnimation: 'fade', 			// fade, slideOpen, none
             captionAnimationSpeed: 600, 		// if so how quickly should they animate in
             bullets: false,						// true or false to activate the bullet navigation
